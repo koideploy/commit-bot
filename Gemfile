0cd6c19c379da1d54955dad118a2ab2d2d0e58ce
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2012-10-22 17:46:26 +1000

    Drop usage of grit and use direct shell commands for git.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source :rubygems
 
-gem 'grit'
 gem 'rspec', :require => 'spec'
 gem 'rake'
 gem 'flickr_fu'
--- lib/commit_bot/repository.rb
+++ lib/commit_bot/repository.rb
@@ -2,17 +2,38 @@ module CommitBot
   class Repository
     LOCAL_PATH = '/tmp/koideploy-showcase'
     REPOSITORY = 'git@github.com:koideploy/showcase.git'
-    CLONE_OPTIONS = {:quiet => true, :verbose => false, :progress => false, :branch => 'master'}
 
     def initialize
       clone
     end
 
     def pull
+      execute_in_working_dir 'git pull'
+    end
+
+    def push
+      execute_in_working_dir 'git push'
+    end
+
+    def commit(photo)
+      execute_in_working_dir 'git add config/flickr_photo.yml'
+      execute_in_working_dir "git commit -m '#{commit_message(photo)}'"
     end
 
     def clone
-      Grit::Git.new('/tmp/stub').clone(CLONE_OPTIONS, REPOSITORY, LOCAL_PATH)
+      unless File.exists?(LOCAL_PATH)
+        `cd /tmp && git clone #{REPOSITORY} koideploy-showcase`
+      end
+    end
+
+    private
+
+    def commit_message(photo)
+      "Display photo \"#{photo.title[0..40]}\" from \"#{photo.owner_username}\""
+    end
+
+    def execute_in_working_dir(command)
+      `cd #{LOCAL_PATH} && #{command}`
     end
   end
 end
