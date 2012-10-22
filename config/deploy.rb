589702e511dd9e62d9ec5773a5a6cf1c6b8ad538
Jason Weathered <jason@jasoncodes.com>
2012-10-22 21:05:22 +1000

    Configure Capistrano for norbert.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,27 @@
 require 'bundler/capistrano'
 require 'capistrano_colors'
 
-set :server_name, ''
-set :user, ''
+set :server_name, 'norbert.odindutton.com'
+set :user, 'koideploy-showcase'
 set :application, 'commit-bot'
-set(:deploy_to) { "~/" }
+set :environment, 'production'
+set(:deploy_to) { "~/apps/#{application}/#{environment}" }
+set :normalize_asset_timestamps, false
 
 set :scm, :git
-set :repository, "git@github.com:koideploy/showcase.git"
+set :repository, "git@github.com:koideploy/commit-bot.git"
 set :branch, "master"
 
 ssh_options[:forward_agent] = true
 set :use_sudo, false
 
 set :whenever_command, 'bundle exec whenever'
-set(:whenever_identifier) { "#{application}-#{rails_env}" }
+set(:whenever_identifier) { "#{application}-#{environment}" }
+set :whenever_roles, :app
 require 'whenever/capistrano'
 
 role :app, server_name
+
+before 'deploy:finalize_update' do
+  run "ln -nfs #{shared_path}/config/* #{release_path}/config/"
+end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-# Use this file to easily define all of your cron jobs.
+job_type :rake, "cd :path && PATH=/usr/local/bin:/usr/bin:/bin bundle exec rake :task --silent :output"
 
 every 30.minutes do
   rake 'commit_bot:work'
