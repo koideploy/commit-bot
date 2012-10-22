f4bf014a768aaff367ec3d51a8058effb3aca8ea
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2012-10-22 15:53:47 +1000

    Add spec helper.

--- spec/commit_bot/bot_spec.rb
+++ spec/commit_bot/bot_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.expand_path(File.dirname(__FILE__)), *%w(.. .. lib commit_bot))
+require 'spec_helper'
 
 describe CommitBot::Bot do
   describe 'initialize' do
---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), *%w(.. lib commit_bot))
