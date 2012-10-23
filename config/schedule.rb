ba6184af5d0472947ba7dd8a1788b78444906000
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2012-10-23 22:22:21 +1000

    Ping showcase.koideploy.com every 5 minutes to keep it alive.

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,3 +3,7 @@ job_type :rake, "cd :path && PATH=/usr/local/bin:/usr/bin:/bin bundle exec rake 
 every 30.minutes do
   rake 'commit_bot:work'
 end
+
+every 5.minutes do
+  command 'curl -sI showcase.koideploy.com > /dev/null'
+end
